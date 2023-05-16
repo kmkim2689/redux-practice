18307743feb1d51e1b90cc226c1c5bfddc76aefe
Kim Gimun <kmkim7575@gmail.com>
2023-05-16 10:49:40 +0900

    [FEAT] vanilla 이용 redux to do list

--- src/index.js
+++ src/index.js
@@ -1,106 +1,192 @@
 import { legacy_createStore as createStore } from "redux";
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-// reducer 함수를 만들어놓는다.(다른 말로 modifier)
-// reducer 함수란, data를 modify하는 함수를 의미한다... redux에서 '유일하게 data를 modify할 수 있는 부분' => 아래 코드에서 구현된 카운트를 1 늘리거나 줄이는 역할을 수행
-// 매개변수로 받아서 그 변수를 처리한 결과를 return할 수 있음.
-// reducer 함수는, createStore의 인자로 들어가게 되면 reducer가 반환하는 값이 application에 있는 data가 된다. => countModifier라는 이름
-// reducer/modifier 함수의 경우 처음으로 data를 바꿔준다.
-// return하는 것은 application에 있는 data. 즉 여기서 application의 데이터는 hello가 됨
-// const countModifier = () => {
-//   return "hello";
+// // 배열을 둠으로써, todo들을 모아놓을 수 있음...
+// const toDos = [];
+
+// const createToDo = toDo => {
+//     // li 태그 요소를 하나 만든다.
+//     const li = document.createElement("ul");
+//     li.innerText = toDo;
+//     // appendChild를 이용하여 ul에 리스트 추가
+//     ul.appendChild(li);
+// };
+
+// const onSubmit = e => {
+//     e.preventDefault();
+//     // todo input에 적힌 값을 가져오고
+//     const toDo = input.value;
+//     // 초기화
+//     input.value = ""
+//     createToDo(toDo);
+// };
+
+// form.addEventListener("submit", onSubmit);
+
+// using redux
+
+// const ADD_TODO = "ADD_TODO";
+// const DELETE_TODO = "DELETE_TODO";
+
+// const addTodo = (text) => {
+//     return {
+//         type: ADD_TODO,
+//         text
+//     }
 // }
 
-// 넘겨받는 매개변수를 지정할 수도 있고, 기본 값을 지정할 수도 있다.
-// const countModifier = (count = 0) => {
-//   // ...modify state...
-//   count++
-//   count--
-//   return count;
+// const deleteTodo = (id) => {
+//     return {
+//         type: DELETE_TODO,
+//         id
+//     }
 // }
 
-// store를 만들어 준다. data를 저장해놓기 위함
-// const countStore = createStore(countModifier);
+// // reducer
+// const reducer = (state = [], action) => {
+//     console.log(action);
+//     // 여기서 state는 직접 수정하는 것이 아니라, 새로운 것을 return함으로서 수정이 이뤄져야 한다.
+//     switch (action.type) {
+//         case ADD_TODO:
+//             // 금지
+//             // return state.push(action.text)
+//             // 새로운 배열을 만들어 리턴하기
+//             // 이 때 spread 연산자를 이용한다. 형태는 ...배열명 혹은 ...객체명모든 객체 혹은 배열의 각 요소 하나하나를 나열하게 된다.
+//             // 즉 이전 array의 contents에 더하여, 새로운 object를 추가
+//             // 즉, state 배열은 text 필드를 가진 객체가 여러개 모인 배열 형태...
+//             // return [...state, { text: action.text, id: Date.now() }];
+//             // 만약 최신의 것이 가장 위로 오게 하고 싶다면 다음과 같이 수정
+//             return [{ text: action.text, id: Date.now() }, ...state];
+//         case DELETE_TODO:
+//             return state.filter(toDo => toDo.id !== action.id);
+//         default:
+//             return state;
+//     }
+// };
 
-// console.log(countStore)를 찍으면, 객체가 반환된다.
-// 객체 안에 있는 것들
-// 1. dispatch
-// 2. subscribe
-// 3. getState
-// 4. replaceReducer
-// + store 변수명.getState() => reducer 함수의 return값을 출력하게 된다.
-// console.log(countStore);
-// console.log(countStore.getState()); // hello 출력
+// const store = createStore(reducer);
 
-// let count = 0;
+// // subscription
+// store.subscribe(() => {
+//     console.log(store.getState())
+// });
 
-// number.innerText = count;
-
-// const updateText = () => {
-//   number.innerText = count;
+// // 보통 dispatch에 들어갈 함수의 경우 분리하여 만든다
+// const dispatchAddTodo = (text) => {
+//     store.dispatch(addTodo(text));
 // }
 
-// const handleAdd = () => {
-//   count += 1;
-//   updateText();
-//   console.log("add");
+// const dispatchDeleteToDo = (e) => {
+//     console.log("delete");
+//     console.log(e.target)
+//     // e.target은 버튼 그 자체를 가리키기 때문에, 부모 요소인 li에서 아이디를 꺼내오기 위하여 parentNode 속성을 사용. 그러면 li를 반환하게 된다.
+//     console.log(e.target.parentNode.id);
+//     const id = parseInt(e.target.parentNode.id);
+//     store.dispatch(deleteTodo(id));
 // }
 
-// const handleMinus = () => {
-//   count -= 1;
-//   updateText();
-//   console.log("minus");
+// const paintToDos = () => {
+//     // store에 변화가 일어날 때마다 리스트의 처음부터 끝까지 렌더링을 진행
+//     const toDos = store.getState();
+//     // ul에 모든 요소를 비움
+//     ul.innerHTML = "";
+//     toDos.forEach((toDo) => {
+//         const li = document.createElement("li");
+//         // 삭제 기능을 추가하기 위하여, delete 버튼을 추가
+//         // btn 클릭에 대한 이벤트 리스너
+//         const btn = document.createElement("button");
+//         btn.addEventListener("click", dispatchDeleteToDo)
+//         btn.innerText = "DEL";
+//         // li의 자식으로 넣는다. li에 삭제 버튼이 속하도록
+//         li.appendChild(btn);
+//         li.id = toDo.id;
+//         li.innerText = toDo.text;
+//         ul.appendChild(li);
+//     })
 // }
 
-// add.addEventListener("click", handleAdd);
-// minus.addEventListener("click", handleMinus);
+// // todo 항목의 변경 시 재렌더링을 하기 위하여, 아래와 같이 subscription을 설정
+// store.subscribe(paintToDos)
 
-number.innerText = "0";
 
-const countModifier = (count = 0, action) => {
-  // modify data
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+
+// const onSubmit = (e) => {
+//     e.preventDefault();
+//     const toDo = input.value;
+//     input.value = "";
+//     // html을 변경하는 대신, dispatch를 이용
+//     // dispatch 인자로 들어가는 객체에는 type뿐만 아니라 유저가 선택적으로 리듀서에 넘겨주고 싶은 내용을 추가로 명시할 수도 있다. 여기서는 todo 리스트에 추가될 내용을 넘겨줘야 하기 때문에 text라는 필드를 추가적으로 넘겨주는 작업을 수행한다.
+//     dispatchAddTodo(toDo);
+// }
+
+// form.addEventListener("submit", onSubmit)
+
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+const addToDo = text => {
+  return {
+    type: ADD_TODO,
+    text
+  };
+};
+
+const deleteToDo = id => {
+  return {
+    type: DELETE_TODO,
+    id
+  };
+};
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [{ text: action.text, id: Date.now() }, ...state];
+    case DELETE_TODO:
+      return state.filter(toDo => toDo.id !== action.id);
+    default:
+      return state;
   }
-  
-}
+};
 
-const countStore = createStore(countModifier);
+const store = createStore(reducer);
 
-// countStore.dispatch({ type: "ADD" });
+store.subscribe(() => console.log(store.getState()));
 
-console.log(countStore.getState());
+const dispatchAddToDo = text => {
+  store.dispatch(addToDo(text));
+};
 
+const dispatchDeleteToDo = e => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteToDo(id));
+};
 
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = "";
+  toDos.forEach(toDo => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", dispatchDeleteToDo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
+};
 
+store.subscribe(paintToDos);
 
-// 버튼 클릭시 이벤트 발생시키는 것. 클릭 이벤트를 설정하는 것이므로 첫번째 인자로 click을 집어넣어야 함
+const onSubmit = e => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  dispatchAddToDo(toDo);
+};
 
-const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
-}
-
-const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
-}
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
-
-// 여기까지만 진행하면, count값은 변경되지만 화면에 반영은 되지 않음
-// 이것을 해결해줄 수 있는 것이 바로 구독(subscription)
-
-// subscription : store 안의 변수들의 변화를 감지하여 알려줌 => store에 구독
-const onChange = () => {
-  console.log(countStore.getState())
-  number.innerText = countStore.getState();
-}
-// countStore 안의 변수의 변화가 발생할 때마다 onChange 함수가 실행되는 것을 확인 가능
-countStore.subscribe(onChange)
+form.addEventListener("submit", onSubmit);
